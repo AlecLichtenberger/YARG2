a9626f975453848b92119ef9be1746d3c8795f9e
Alec Lichtenberger <lichtenbergeralec@gmail.com>
2025-04-24 11:23:13 -0700

    Trying a new file system

--- YARG2/Assets/Scripts/MainMenu.cs
+++ YARG2/Assets/Scripts/MainMenu.cs
@@ -1,26 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
 public class MainMenu : MonoBehaviour
 {
-    public void startGame()
+    public void StartGame()
     {
+        Debug.Log("StartGame button clicked");
         SceneManager.LoadScene(1);
     }
 
-    public void quitGame()
+    public void QuitGame()
     {
         Application.Quit();
-    }
-
-    public void displayCredits()
-    {
-
-    }
-    
-    public void displaySettings()
-    {
-        
     }
 
 }
